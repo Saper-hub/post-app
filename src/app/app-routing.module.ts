3b65672cf8755d601e7b6bd5a06f7963782e2047
Uzytkownik <uzytkownik@kursGIT.pl>
2020-09-13 15:47:52 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './component/home/home.component';
+import {PostsListComponent} from './component/posts-list/posts-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path: '',
+   component: HomeComponent
+  },
+  {
+    path: 'posts',
+    component: PostsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/posts-list/posts-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Post} from '../../model/post.model';
+import {PostsService} from '../../service/posts.service';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css']
+})
+export class PostsListComponent implements OnInit {
+
+  public posts: Post[] = [];
+
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.postsService.readPosts().subscribe(
+      (posts: Post[]) =>
+    {
+      this.posts = posts;
+    });
+  }
+
+}
